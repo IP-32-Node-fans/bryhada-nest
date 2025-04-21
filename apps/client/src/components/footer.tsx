1628999984bba7f407cf1306fed3495687323274
Kyryloloshka <kirill.contrmptuous@gmail.com>
2025-04-21 14:54:04 +0300

    feat(client): move home page

---
+++ apps/client/src/components/footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container footer-container">
+        <h5>
+          Розроблено в межах курсу &apos;Основи розроблення ПЗ на платформі
+          <a href="https://nodejs.org" target="_blank" className="node-link"
+            >Node.js</a>&apos;
+        </h5>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
